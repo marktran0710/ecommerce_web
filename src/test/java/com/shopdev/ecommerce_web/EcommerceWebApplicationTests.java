ef99ca448d6127269f60702f85730f3dfb291bc9
Hau Tran <tranleconghau772001@gmail.com>
2024-09-01 17:36:13 +0700

    init

---
+++ src/test/java/com/shopdev/ecommerce_web/EcommerceWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopdev.ecommerce_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
