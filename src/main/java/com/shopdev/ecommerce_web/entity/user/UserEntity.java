9f2fc574a193a0e71ca4d9b2fee8cfefb474e5fa
Hau Tran <tranleconghau772001@gmail.com>
2024-09-05 07:28:50 +0700

    hibernate annotation

--- src/main/java/com/shopdev/ecommerce_web/entity/user/UserEntity.java
+++ src/main/java/com/shopdev/ecommerce_web/entity/user/UserEntity.java
@@ -1,13 +1,24 @@
 package com.shopdev.ecommerce_web.entity.user;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
+@Data
 @Entity
+@Table(name = "java_user_001")
+@DynamicInsert
+@DynamicUpdate
 public class UserEntity {
 
     @Id
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // userId
+
+    @Column(columnDefinition = "varchar(255) comment 'user name'", nullable = false)
     private String username;
+
+    @Column(columnDefinition = "varchar(255) comment 'user email'", nullable = false, unique = true)
     private String userEmail;
 }
