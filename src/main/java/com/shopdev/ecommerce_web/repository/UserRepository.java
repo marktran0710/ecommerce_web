3f35586fbb66f3cd7fc45be7a373c5a922671f83
Hau Tran <tranleconghau772001@gmail.com>
2024-09-07 22:39:43 +0700

    continue ÆÆwith Ãuser jpa

--- src/main/java/com/shopdev/ecommerce_web/repository/UserRepository.java
+++ src/main/java/com/shopdev/ecommerce_web/repository/UserRepository.java
@@ -5,4 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
+    UserEntity findByUserNameAndUserEmail(String userName, String userEmail);
+
+    UserEntity findByUserNam(String userName);
 }
