90f6589a9cf864ae33fdc1488eef81c28b13d946
Hau Tran <tranleconghau772001@gmail.com>
2024-09-09 06:55:44 +0700

    user jpa continuing

--- src/main/java/com/shopdev/ecommerce_web/repository/UserRepository.java
+++ src/main/java/com/shopdev/ecommerce_web/repository/UserRepository.java
@@ -1,11 +1,45 @@
 package com.shopdev.ecommerce_web.repository;
 
 import com.shopdev.ecommerce_web.entity.user.UserEntity;
+import jakarta.transaction.Transactional;
+import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long>, JpaSpecificationExecutor<UserEntity> {
 
     UserEntity findByUserNameAndUserEmail(String userName, String userEmail);
 
     UserEntity findByUserNam(String userName);
+
+    List<UserEntity> findByUserNameStartingWith(String userEmail);
+
+    List<UserEntity> findByUserNameEndingWith(String userEmail);
+
+    List<UserEntity> findByIdLessThan(Long id);
+
+    // RAW
+    @Query("SELECT u FROM UserEntity u WHERE u.id = (SELECT(MAX(p.id) FROM UserEntity p)")
+    UserEntity findMaxIdUser();
+
+    @Query("SELECT u FROM UserEntity u WHERE u.userName = ?1 AND u.userEmail = ?2")
+    List<UserEntity> getUserEntityBy( String userName, String userEmail);
+
+    @Query("SELECT u FROM UserEntity u WHERE u.userName = :userName AND u.userEmail = :userEmail")
+    List<UserEntity> getUserEntityByTwo(@Param("userNam") String userName, @Param("userEmail") String userEmail);
+
+    //    UPDATE - DELETE
+    @Modifying
+    @Query("UPDATE UserEntity u SET u.userEmail = :userName")
+    @Transactional
+    int updateUserName(@Param("userName") String userName);
+
+    // native query
+    @Query(value = "SELECT COUNT(id) FROM java_user_001", nativeQuery = true)
+    long getTotalUser();
 }
